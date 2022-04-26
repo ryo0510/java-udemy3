0171068799c083481f04c87be7b9b8ada4ce50f0
河野涼 <ryokawano@kounoryounoMacBook-Pro.local>
2022-04-26 16:40:04 +0900

    (finish)2-22

---
+++ Sample.java
@@ -0,0 +1,11 @@
+package jp.learningdesign.javastudy;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello Eclipse!");
+
+	}
+
+}
